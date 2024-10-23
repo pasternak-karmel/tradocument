517b535303d3b6f8162b9d5c74c814092078cef9
pasternak-karmel <101138016+pasternak-karmel@users.noreply.github.com>
2024-10-23 21:07:00 +0100

    Update index.ts

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,4 @@
 import * as z from "zod";
-import { UserRole } from "@prisma/client";
 
 export const SettingsSchema = z
   .object({
@@ -77,42 +76,4 @@ export const CreateLivreurSchema = z.object({
   email: z.string().email({
     message: "Email is required",
   }),
-});
-
-export const AddArticleSchema = z.object({
-  nom: z
-    .string()
-    .min(2, { message: "Le nom doit contenir au moins 2 caractères" })
-    .max(50, { message: "Le nom ne peut pas dépasser 50 caractères" }),
-  prix: z.coerce.number().min(0, "Price must be greater than or equal to 0"),
-  quantite: z.coerce.number().min(1, "Quantity be greater than or equal to 0"),
-  usage: z.boolean().default(false).optional(),
-  description: z.string().max(1500, "max caracters allowed is 1500"),
-  categories: z.enum([
-    "outils",
-    "meubles",
-    "jardin",
-    "Electroménager",
-    "pour la maison",
-    "jeux videos",
-    "livre films et musique",
-    "bijoux et accessoires",
-    "sac et bagages",
-    "vetements et chaussures pour hommes",
-    "vetements et chaussures pour femmes",
-    "jouer et jeux",
-    "puericulture et enfants",
-    "sante et beaute",
-    "telephones mobiles",
-    "electroniques et ordinateurs",
-    "sports et activites exterieures",
-    "instruments de musique",
-    "artisanat d'art",
-    "antiquites et objects de collection",
-    "pieces automobiles",
-    "velos",
-    "vide-grenier",
-    "divers",
-  ]),
-  image: z.array(z.string()).optional(),
 });
