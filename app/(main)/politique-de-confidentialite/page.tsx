55b489f36e66c879d332fcb9b6efb43a9ed9ba6f
Carmel <136585899+Carmel1607@users.noreply.github.com>
2024-11-11 18:53:37 +0100

    Update page.tsx

--- app/(main)/politique-de-confidentialite/page.tsx
+++ app/(main)/politique-de-confidentialite/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
+
 
 export default function Component() {
   return (
