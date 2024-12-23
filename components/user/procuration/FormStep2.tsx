df2363ab7ae5ea66d7c957bd5237e7aebb372587
Karmel AVENON <karmelavenon@gmail.com>
2024-12-23 22:08:48 +0100

    Update FormStep2.tsx

--- components/user/procuration/FormStep2.tsx
+++ components/user/procuration/FormStep2.tsx
@@ -54,24 +54,136 @@ export const FormStep2: React.FC<FormStep2Props> = ({ form, loading }) => {
                 <MultiSelectorItem value={"Acte de naissance"}>
                   Acte de naissance
                 </MultiSelectorItem>
-                <MultiSelectorItem value={"Passeport"}>
-                  Passeport
+                <MultiSelectorItem value={"Acte de mariage"}>
+                  Acte de mariage
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Acte de décès"}>
+                  Acte de décès
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Acte de Naturalisation/Nationalité"}>
+                  Acte de Naturalisation/Nationalité
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Acte de reconnaissance"}>
+                  Acte de reconnaissance
                 </MultiSelectorItem>
                 <MultiSelectorItem value={"Carte d'identité"}>
                   Carte d'identité
                 </MultiSelectorItem>
+                <MultiSelectorItem value={"Certificat de célibat"}>
+                  Certificat de célibat
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Certificat de changement de nom"}>
+                  Certificat de changement de nom
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Déclaration sur l'honneur"}>
+                  Déclaration sur l'honneur
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Livret de famille"}>
+                  Livret de famille
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Passeport"}>
+                  Passeport
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Permis de séjour"}>
+                  Permis de séjour
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Procuration"}>
+                  Procuration
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Bilan"}>Bilan</MultiSelectorItem>
+                <MultiSelectorItem value={"Contrat"}>Contrat</MultiSelectorItem>
+                <MultiSelectorItem value={"Convention"}>
+                  Convention
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Acte de divorce"}>
+                  Acte de divorce
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Acte notarié"}>
+                  Acte notarié
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Assignations et Jugements"}>
+                  Assignations et Jugements
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Casier Judiciaire"}>
+                  Casier Judiciaire
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Exequatur"}>
+                  Exequatur
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Procuration Juridique"}>
+                  Procuration Juridique
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Testament"}>
+                  Testament
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Attestation de scolarité"}>
+                  Attestation de scolarité
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Attestation de stage"}>
+                  Attestation de stage
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Attestation d'études"}>
+                  Attestation d'études
+                </MultiSelectorItem>
+                <MultiSelectorItem
+                  value={"Diplôme (Licence, Ingénieurat, Master, Doctorat...)"}
+                >
+                  Diplôme (Licence, Ingénieurat, Master, Doctorat...)
+                </MultiSelectorItem>
+                <MultiSelectorItem
+                  value={
+                    "Relevé de notes Bac/Relevé de notes universitaires avec appréciations"
+                  }
+                >
+                  Relevé de notes Bac/Relevé de notes universitaires avec
+                  appréciations
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Avis d'imposition"}>
+                  Avis d'imposition
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Bulletin de salaire"}>
+                  Bulletin de salaire
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Relevé de compte"}>
+                  Relevé de compte
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Appels d'offre"}>
+                  Appels d'offre
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Brevets"}>Brevets</MultiSelectorItem>
+                <MultiSelectorItem value={"Brochures"}>
+                  Brochures
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Cahier des charges"}>
+                  Cahier des charges
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Catalogues Produits"}>
+                  Catalogues Produits
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Journaux d'entreprise"}>
+                  Journaux d'entreprise
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Manuels et notices techniques"}>
+                  Manuels et notices techniques
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Newsletters spécialisées"}>
+                  Newsletters spécialisées
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Normes"}>Normes</MultiSelectorItem>
+                <MultiSelectorItem value={"Rapport d'expertise"}>
+                  Rapport d'expertise
+                </MultiSelectorItem>
+                <MultiSelectorItem value={"Rapport technique"}>
+                  Rapport technique
+                </MultiSelectorItem>
               </MultiSelectorList>
             </MultiSelectorContent>
           </MultiSelector>
-          {/* <Input
-            id="documents"
-            placeholder="Documents"
-            {...register("documents")}
-          /> */}
           {errors.documents && (
             <p className="text-red-500 text-sm">{errors.documents.message}</p>
           )}
         </div>
+
         <div>
           <Label htmlFor="institution">
             Institution ou Lieu de récupération
